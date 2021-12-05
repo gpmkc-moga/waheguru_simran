ef6acbfc88d538275e23c8d07b902ab67d561971
Jasdeep Singh <cingh.jasdeep@gmail.com>
2021-12-05 15:47:31 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/waheguru_simran/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.waheguru_simran
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WaheguruSimranApp());
+}
+
+class WaheguruSimranApp extends StatelessWidget {
+  const WaheguruSimranApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const WaheguruSimranHomePage(),
+    );
+  }
+}
+
+class WaheguruSimranHomePage extends StatefulWidget {
+  const WaheguruSimranHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WaheguruSimranHomePage> createState() => _WaheguruSimranHomePageState();
+}
+
+class _WaheguruSimranHomePageState extends State<WaheguruSimranHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
